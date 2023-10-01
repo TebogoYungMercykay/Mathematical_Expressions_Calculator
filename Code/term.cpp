e2e0a318e8864918523d968677a40b1f77511410
TebogoYungMercykay <sbkskhalo.kq@gmail.com>
2023-10-01 19:27:05 +0200

    Removed Random TextFile, Implemented 2 Operators

--- Code/term.cpp
+++ Code/term.cpp
@@ -126,6 +126,61 @@ term::term(int c, int n, char* v, int* p) {
 }
 
 term::term(const char* input) {
+    std::string str(input);
+    int pos = 0;
+    if (str[0] == '-') {
+        this->coefficient = -1;
+        pos++;
+    } else if (str[0] == '+') {
+        this->coefficient = 1;
+        pos++;
+    } else {
+        this->coefficient = 1;
+    }
+
+    while (pos < str.length() && isdigit(str[pos])) {
+        this->coefficient = this->coefficient * 10 + (str[pos] - '0');
+        pos++;
+    }
+
+    this->numVariables = 0;
+    this->variables = new char[26];
+    this->powers = new int[26];
+
+    while (pos < str.length()) {
+        if (str[pos] == '*') {
+            pos++;
+            continue;
+        }
+
+        char var = str[pos];
+        int pow = 0;
+        pos++;
+
+        if (pos < str.length() && str[pos] == '^') {
+            pos++;
+            while (pos < str.length() && isdigit(str[pos])) {
+                pow = pow * 10 + (str[pos] - '0');
+                pos++;
+            }
+        } else {
+            pow = 1;
+        }
+
+        for (int i = 0; i < this->numVariables; i++) {
+            if (this->variables[i] == var) {
+                this->powers[i] += pow;
+                var = '\0';
+                break;
+            }
+        }
+
+        if (var != '\0') {
+            this->variables[this->numVariables] = var;
+            this->powers[this->numVariables] = pow;
+            this->numVariables++;
+        }
+    }
 }
 
 term& term::operator=(const term& other) {
@@ -144,23 +199,47 @@ int term::getNumVariables() const {
 }
 
 int term::getDegree() const {
-    return 0 /*this->*/;
+    int degree = 0;
+    for (int i = 0; i < this->numVariables; i++) {
+        degree += this->powers[i];
+    }
+    return degree;
 }
 
 int term::getVarIndex(char var) const {
-    for (int i = 0; i < numVariables; i++) {
-        if (variables[i] == var) {
+    for (int i = 0; i < this->numVariables; i++) {
+        if (this->variables[i] == var) {
             return i;
         }
     }
     return -1;
 }
 
-string term::operator~() const {
-    return "";
+std::string term::operator~() const {
+    std::ostringstream os;
+    if (this->coefficient == -1) {
+        os << "-";
+    } else if (this->coefficient != 1) {
+        os << this->coefficient;
+    }
+
+    for (int i = 0; i < this->numVariables; i++) {
+        os << "*" << this->variables[i];
+        if (this->powers[i] != 1) {
+            os << "^" << this->powers[i];
+        }
+    }
+
+    std::string str = os.str();
+    if (str[0] == '*') {
+        str.erase(0, 1);
+    }
+
+    return str;
 }
 
-ostream& operator<<(ostream& os, const term& t) {
+std::ostream& operator<<(std::ostream& os, const term& t) {
+    os << ~t << std::endl;
     return os;
 }
 
